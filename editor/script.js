2c4fac6d5e6349081e73638d7acf8bbb3a5bd26c
HoorayTritium <48468687+HoorayTritium@users.noreply.github.com>
2020-11-27 21:43:42 +0900

    Add files via upload

---
+++ editor/script.js
@@ -0,0 +1,190 @@
+var SimpleImages = ""
+
+let saveData;
+let editor
+getSaveData.then((value) => {
+    // editorの設定
+    editor = new EditorJS({
+        holder: 'editor',
+        autofocus: true,
+        tools: {
+            header: Header,
+            list: List,
+            delimiter: Delimiter,
+            sImage: {
+                class: SimpleImage,
+                inlineToolbar: true,
+                render() {
+                    const input = document.createElement('input');
+                    input.value = this.data && this.data.url ? this.data.url : '';
+                    return input;
+                }
+            },
+            image: {
+                class: ImageTool,
+                config: {
+                    uploader: {
+                        /**
+                        * Upload file to the server and return an uploaded image data
+                        * @param {File} file - file selected from the device or pasted by drag-n-drop
+                        * @return {Promise.<{success, file: {url}}>}
+                        */
+                        uploadByFile(file) {
+                            return ImageUpload(file).then((result) => {
+                                return result;
+                            })
+                        },
+                        uploadByUrl(url) {
+                            return new Promise((resolve) => {
+                                resolve({
+                                    success: 1,
+                                    file: {
+                                        url: url
+                                    }
+                                })
+                            });
+                        }
+                    }
+                },
+                render() {
+                    const input = document.createElement('input');
+                    input.value = this.data && this.data.url ? this.data.url : '';
+                    return input;
+                }
+            }
+            // checklist: Checklist,
+            // quote: Quote,
+            // code: CodeTool
+        },
+        data: value
+    });
+})
+
+
+
+$(window).on('load', function () {
+    if (localStorage.getItem('data')) {
+        // editor.configuration.data = JSON.parse(localStorage.getItem('data'))
+    }
+});
+
+// 入力内容を一時保存
+const editorCheck = document.getElementById('editor')
+addMultipleEventListener(editorCheck, "input paste", () => {
+    editor.save().then((data) => {
+        let title = ""
+        let subTilte = ""
+        let text = ""
+        let img = ""
+        let cnt = 0
+        data.blocks.forEach(e => {
+            // listいる？
+            switch (e.type) {
+                case "paragraph":
+                    text += e.data.text + "\n"
+                    break;
+                case "header":
+                    let i = e.data.level
+                    switch (i) {
+                        case 2:
+                            title = e.data.text
+                            break;
+                        case 3:
+                            subTilte = e.data.text
+                            break;
+                        default:
+                            text += `<h${i}>${e.data.text}</h${i}>`
+                            break;
+                    }
+                    break;
+                case "delimiter":
+                    text += "<hr>"
+                    break;
+                // base64形式の場合、処理を考える
+                case "image":
+                    if (!e.data.file.url.match(new RegExp('data.*base64,'))) {
+                        text += "$image" + cnt++
+                        img += e.data.file.url + ','
+                    }
+                    break;
+                case "sImage":
+                    if (!e.data.url.match(new RegExp('data.*base64,'))) {
+                        text += "$image" + cnt++
+                        img += e.data.url + ','
+                    }
+                    break;
+                default:
+                    break;
+            }
+        })
+        // console.log(text, img)
+        localStorage.setItem('title', title)
+        localStorage.setItem('subTitle', subTilte)
+        localStorage.setItem('text', text)
+        localStorage.setItem('image', img)
+        localStorage.setItem('data', JSON.stringify(data))
+    })
+})
+
+// clear
+const clear = document.getElementById("clear")
+clear.addEventListener('click', () => {
+    editor.clear()
+})
+// ボタンを押したら保存
+const save = document.getElementById('save');
+const number = getArticleNumber()
+const password = document.getElementById('password')
+save.addEventListener('click', () => {
+    editor.save().then((outputData) => {
+        // ここでデータ形式を整えて送信
+        outputData.blocks.forEach(element => {
+            console.log(element)
+        });
+        $.ajax({
+            method: 'POST',
+            url: 'https://script.google.com/macros/s/AKfycbwUac0ElurSoUpRNpduWY_4JDF_yeeWLQSwBk_DIsI/dev',
+            data: {
+                title: localStorage.getItem("title"),
+                subTitle: localStorage.getItem("subTitle"),
+                text: localStorage.getItem("text"),
+                image: localStorage.getItem("image"),
+                request: 'newArticle',
+                number: number,
+                password: password.value
+            },
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            jsonpCallback: 'displayData',
+            crossDomain: true,
+        })
+    }).catch((error) => {
+        console.log('Saving failed: ', error)
+    });
+});
+function displayData(data) {
+    console.log(data)
+}
+
+// 画像をbase64に変換
+function toBase64Url(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            callback(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+}
+
+// ページを離れる前に警告
+$(function () {
+    $(window).on('beforeunload', function () {
+        return "このページを離れると、入力したデータが削除されます。修正の場合には、「修正ボタン」をクリックしてください。";
+    });
+});
+
---
+++ editor/setup.js
@@ -0,0 +1,134 @@
+const addMultipleEventListener = (target, eventNames, listener) => {
+    const events = eventNames.split(" ");
+    events.forEach(event => target.addEventListener(event, listener, false));
+};
+
+Reflect.defineProperty(EventTarget.prototype, "on", {
+    configurable: true,
+    enumerable: false,
+    writable: true,
+    value: function (eventNames, listener) {
+        const events = eventNames.split(" ");
+        events.forEach(event => this.addEventListener(event, listener, false));
+    },
+});
+
+const getUrlQueries = () => {
+    let queryStr = window.location.search.slice(1);  // 文頭?を除外
+    queries = {};
+
+    // クエリがない場合は空のオブジェクトを返す
+    if (!queryStr) {
+        return queries;
+    }
+
+    // クエリ文字列を & で分割して処理
+    queryStr.split('&').forEach((queryStr) => {
+        // = で分割してkey,valueをオブジェクトに格納
+        let queryArr = queryStr.split('=');
+        queries[queryArr[0]] = queryArr[1];
+    });
+
+    return queries;
+}
+
+const getArticleNumber = () => {
+    let n = getUrlQueries().n;
+    return isNaN(n) || n == 0 ? -1 : n;
+}
+
+// 画像をimgurにアップロード
+const ImageUpload = (file, isBase64 = false) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (event) => {
+            console.log(event)
+            const base64 = reader.result.replace(new RegExp('data.*base64,'), '');
+            $.ajax({
+                url: "https://api.imgur.com/3/image",
+                type: 'POST',
+                headers: {
+                    "Authorization": 'Client-ID a23f3ab17570a8b'
+                },
+                data: {
+                    image: base64,
+                    type: 'base64',
+                }
+            }).done((res) => {
+                console.log(res)
+                resolve({
+                    success: 1,
+                    file: {
+                        url: res.data.link
+                    }
+                })
+            }).fail((err) => {
+                console.error("error: \n" + err)
+                reject({
+                    success: 0,
+                    file: {
+                        url: ""
+                    }
+                })
+            })
+        }
+    })
+}
+
+var hoge;
+const getArticle = new Promise((resolve) => {
+    $.ajax({
+        method: 'POST',
+        url: 'https://script.google.com/macros/s/AKfycbwUac0ElurSoUpRNpduWY_4JDF_yeeWLQSwBk_DIsI/dev',
+        data: {
+            request: 'getAll',
+        },
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        jsonpCallback: 'getArticleCallback',
+        crossDomain: true,
+    }).done((e) => {
+        console.log("hoge\n" + e)
+    }).fail((e) => {
+        console.error(e)
+        // console.log(saveData)
+        if (e.status === 200) {
+            resolve(saveData)
+        }
+    })
+})
+// 取得したデータを基に描画 or 表示
+// 明日は記事ナンバーからエディターに登録するところをやる
+const getArticleCallback = (data) => {
+    console.log(data.meta)
+    hoge = (data.data)
+    saveData = { time: new Date(), blocks: [], version: "2.19.0" }
+    let preData = data.data[getArticleNumber()]
+    saveData.blocks[0] = { type: "header", data: { level: 2, text: preData.title } }
+    saveData.blocks[1] = { type: "header", data: { level: 3, text: preData.subTitle } }
+    let i = 2
+    const imgPattern = /image[\d]{1}/
+    preData.main.split('$').forEach(e => {
+        if (imgPattern.test(e)) {
+            let imgNum = e.match(/[\d]{1}/)
+            saveData.blocks[i++] = { type: "sImage", data: { url: preData.images[imgNum] } }
+        } else
+            saveData.blocks[i++] = { type: "paragraph", data: { text: e } }
+    })
+    return saveData
+}
+
+const getSaveData = new Promise((resolve, reject) => {
+    let n = getArticleNumber()
+    if (n > 0) {
+        getArticle.then((val) => {
+            console.log(val)
+            localStorage.setItem("data", JSON.stringify(saveData))
+            resolve(val)
+        })
+    } else {
+        console.log('saved data')
+        resolve(JSON.parse(localStorage.getItem("data")))
+    }
+})
